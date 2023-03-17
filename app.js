f125bb0a211d88b03a01c0df76e7a6c4206cad5f
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-17 15:59:30 +0530

    shows links on click of navToggle

--- app.js
+++ app.js
@@ -7,7 +7,13 @@
 const date = document.querySelector('#date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', ()=>{
+    linksContainer.classList.toggle('show-links');
+})
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
